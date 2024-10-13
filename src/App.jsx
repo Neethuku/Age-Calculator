f53c03219a0326c306a144649028b4e73b64d9cf
Neethu ku <neethuku02@gmail.com>
2024-10-13 23:45:51 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   const [birthDate, setBirthDate] = useState('');
